dffb0ceb978492f3259121165d6099e979f7c21e
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2009-10-30 11:18:08 -0400

    updated mimes

--- nimble_support/lib/mime.php
+++ nimble_support/lib/mime.php
@@ -11,6 +11,20 @@
 	const JS = 'text/javascript';
 	/** CSS */
 	const CSS = 'text/css';
+	
+	const JPG = 'image/jpeg';
+	
+	const JPEG = self::JPG;
+	
+	const PNG = 'image/png';
+	
+	const GIF = 'image/gif';
+	
+	const PDF = 'application/pdf';
+	
+	const SVG = 'image/svg+xml';
+	
+	const BMP = 'image/bmp';
   }
  
  ?>
