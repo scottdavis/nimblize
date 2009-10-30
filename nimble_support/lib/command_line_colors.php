fac4f872947d8019ed417aec917277d055a90f81
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2009-10-30 09:49:03 -0400

    changes tests etc

---
+++ nimble_support/lib/command_line_colors.php
@@ -0,0 +1,30 @@
+<?php
+	
+	class CommandlineColors {
+			private static $colors = array(
+		    "black" => 0,
+		    "red" => 1,
+		    "green" => 2,
+		    "yellow" => 3,
+		    "blue" => 4,
+		    "magenta" => 5,
+		    "cyan" => 6,
+		    "white" => 7
+				);
+	
+			private static $formats = array(
+    		"bold" => 1,
+    		"underline" => 4
+  			);
+
+			// self::blue_on_white
+			public static __callStatic($method, $args) {
+				$method = strtolower($method);
+				if(strpos('on', $method) !== false) {
+					
+				}
+			}
+
+		
+
+?>
