0d6bbca5a02a39450892f0e26d9491a363b828d0
John Bintz <bintz@flux.stsci.edu>
2009-11-04 11:12:21 -0500

    case changes

--- test/nimble_record/config.php
+++ test/nimble_record/config.php
@@ -4,8 +4,8 @@
 	require_once(dirname(__FILE__) . '/../../nimblize.php');
 	require_once(dirname(__FILE__) . '/../../nimble_record/migrations/migration.php');
 	require_once(dirname(__FILE__) . '/../../nimble_record/migrations/lib/migration_runner.php');
-	require_once(dirname(__FILE__) . '/model/user.php');
-	require_once(dirname(__FILE__) . '/model/photo.php');
+	require_once(dirname(__FILE__) . '/model/User.php');
+	require_once(dirname(__FILE__) . '/model/Photo.php');
 	$settings = array('host' 			=> 'localhost',
 										'database' 	=> MYSQL_DATABASE,
 										'username'	=> 'root',
