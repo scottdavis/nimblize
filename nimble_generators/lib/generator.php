c7393b4711ac84d0a2462e01934e9affe4c161e8
John Bintz <john@coswellproductions.com>
2009-11-11 11:56:31 -0500

    generator cleanup

--- nimble_generators/lib/generator.php
+++ nimble_generators/lib/generator.php
@@ -1,88 +1,92 @@
 <?php
-	require_once(dirname(__FILE__) . '/../../nimble_support/lib/file_utils.php');
-	require_once(dirname(__FILE__) . '/../../nimble_support/lib/inflector.php');
+require_once(dirname(__FILE__) . '/../../nimble_support/lib/file_utils.php');
+require_once(dirname(__FILE__) . '/../../nimble_support/lib/inflector.php');
+
+/**
+ * Class for generating the components of a Nimble skeleton.
+ * @package Generators
+ */
+class Generator {
+	public static $template_path, $script_path, $nimble_root;
 
 	/**
-	 * Class for generating the components of a Nimble skeleton.
-	 * @package Generators
+	 * Handle generating templates magically.
 	 */
-	class Generator {
-		public static $template_path, $script_path, $nimble_root;
-		
-		/**
-		 * Handle generating templates magically.
-		 */
-		public static function __callStatic($method, $arguments) {
-			if (strpos($method, 'generate_') === 0) {
-			  $template_path = FileUtils::join(static::$template_path, str_replace('generate_', '', $method) . '.tmpl');
-			  if (file_exists($template_path)) {			  	
-			  	if (isset($arguments[0])) {
-  					copy($template_path, $arguments[0]);
-			  	}
-			  }
-			}	
-		}
-
-		/**
-		 * Creates the database config files for a specified environment.
-		 * @param string $path The path where the config file should be generated.
-		 * @param string $env The name of the environment to generate.
-		 */
-		public static function generate_database_config($path, $env) {
-			$string = str_replace('[env]', $env, file_get_contents(FileUtils::join(static::$template_path, 'database.json')));
-			static::write_file($path, $string);
-		}
-	
-		/**
-		 * Copy global user scripts into the target script directory.
-		 * @param string $path The target path for scripts. Will not be generated if it doesn't exist.
-		 */
-		public static function generate_scripts($path) {
-			if (!is_dir($path)) { throw new Exception("Target directory does not exist: ${path}"); }
-			
-			$ignore = array('nimblize');
-			if ($dir = opendir(static::$script_path)){
-				while (($file = readdir($dir)) !== false) {
-					if (in_array($file, $ignore)) { continue; }
-					$source_file = FileUtils::join(static::$script_path, $file);
-					if (is_file($source_file)) {
-						$target_file = FileUtils::join($path, $file);
-						copy($source_file, $target_file);
-						@chmod($target_file, 0775);
-					}
+	public static function __callStatic($method, $arguments) {
+		if (strpos($method, 'generate_') === 0) {
+			$template_path = FileUtils::join(static::$template_path, str_replace('generate_', '', $method) . '.tmpl');
+			if (file_exists($template_path)) {
+				if (isset($arguments[0])) {
+					copy($template_path, $arguments[0]);
 				}
-				closedir($dir);
-			} else {
-			  throw new Exception("Source directory cannot be read: " . static::$script_path);	
 			}
 		}
+	}
 
-		/**
-		 * Generate a test case.
-		 * @param string $type The type of test to generate.
-		 * @param string $name The class name for the generated test.
-		 */
-		public static function generate_test($type, $name) {
-			$class_name = Inflector::classify($name);
-			$path = FileUtils::join(static::$nimble_root, 'test', $type);
-			
-			if (is_dir($path)) {
-  			$test_path = 'nimblize/nimble_test/lib/phpunit_testcase.php';
-				if (($test_case_code = file_get_contents(FileUtils::join(static::$template_path, "${type}_test.tmpl"))) !== false) {
-					$test_case_code = str_replace(
-						array('{class_name}', '{test_path}'),
-						array($class_name, $test_path),
-						$test_case_code
-					);
-				} else {
-					throw new Exception("Test source file for ${type} not found!");	
-				}
-				
-				FileUtils::mkdir_p($path);
-				$file_path = FileUtils::join($path, $class_name . 'Test.php');
-				static::write_file($file_path, $test_case_code);				
+	/**
+	 * Creates the database config files for a specified environment.
+	 * @param string $path The path where the config file should be generated.
+	 * @param string $env The name of the environment to generate.
+	 */
+	public static function generate_database_config($path, $env) {
+		$string = str_replace('[env]', $env, file_get_contents(FileUtils::join(static::$template_path, 'database.json')));
+		static::write_file($path, $string);
+	}
+
+	/**
+	 * Copy global user scripts into the target script directory.
+	 * @param string $path The target path for scripts. Will not be generated if it doesn't exist.
+	 */
+	public static function generate_scripts($path) {
+		if (!is_dir($path)) {
+			throw new Exception("Target directory does not exist: ${path}");
+	}
+
+	$ignore = array('nimblize');
+	if ($dir = opendir(static::$script_path)){
+		while (($file = readdir($dir)) !== false) {
+			if (in_array($file, $ignore)) { continue; }
+			$source_file = FileUtils::join(static::$script_path, $file);
+			if (is_file($source_file)) {
+				$target_file = FileUtils::join($path, $file);
+				copy($source_file, $target_file);
+				@chmod($target_file, 0775);
+			}
+		}
+		closedir($dir);
+	} else {
+		// @codeCoverageIgnoreStart
+		throw new Exception("Source directory cannot be read: " . static::$script_path);
+		// @codeCoverageIgnoreEnd
+	}
+}
+
+/**
+ * Generate a test case.
+ * @param string $type The type of test to generate.
+ * @param string $name The class name for the generated test.
+ */
+public static function generate_test($type, $name) {
+	$class_name = Inflector::classify($name);
+	$path = FileUtils::join(static::$nimble_root, 'test', $type);
+
+	if (is_dir($path)) {
+		$test_path = 'nimblize/nimble_test/lib/phpunit_testcase.php';
+		if (($test_case_code = file_get_contents(FileUtils::join(static::$template_path, "${type}_test.tmpl"))) !== false) {
+			$test_case_code = str_replace(
+			array('{class_name}', '{test_path}'),
+			array($class_name, $test_path),
+			$test_case_code
+			);
+		} else {
+			throw new Exception("Test source file for ${type} not found!");
+}
+
+FileUtils::mkdir_p($path);
+$file_path = FileUtils::join($path, $class_name . 'Test.php');
+static::write_file($file_path, $test_case_code);
 			} else {
-				throw new Exception("Test directory for ${type} not found!");	
+				throw new Exception("Test directory for ${type} not found!");
 			}
 		}
 
@@ -93,7 +97,7 @@
 		public static function generate_model($name) {
 			$class_name = Inflector::classify($name);
 			$path = FileUtils::join(static::$nimble_root, 'app', 'model', Inflector::underscore($class_name) . '.php');
-			$string = file_get_contents(FileUtils::join(static::$template_path, 'model.tmpl'));	
+			$string = file_get_contents(FileUtils::join(static::$template_path, 'model.tmpl'));
 			$string = str_replace('{class_name}', $class_name, $string);
 			static::write_file($path, $string);
 			static::generate_test('unit', $name);
@@ -124,7 +128,7 @@
 
 		/**
 		 * @access private
- 		 * @return string The methods for the view.
+		 * @return string The methods for the view.
 		 * @see function controller
 		 * @param string $view_path - folder in which to create files
 		 */
@@ -134,50 +138,52 @@
 				self::view(FileUtils::join($view_path, $view . '.php'));
 				$out .= self::view_function($view);
 			}
-	
+
 			foreach(array('create') as $view){
 				$out .= self::view_function($view);
 			}
-	
-	
+
+
 			foreach(array('update', 'delete') as $view) {
 				$out .= self::view_function($view, true);
 			}
-	
-	
+
+
 			foreach(array('show', 'edit') as $view) {
 				self::view(FileUtils::join($view_path, $view . '.php'));
 				$out .= self::view_function($view, true);
 			}
 			return $out;
 		}
-	
+
 		/**
 		 * @param string $action - name of the action
 		 * @param boolean $id - wither created function takes an id or not
 		 * @see function create_view_functions
-		 * @return string 
+		 * @return string
 		 */
 		public static function view_function($action, $id = false) {
-      $out = array();
+			$out = array();
 
-      $out[] = '  /**';
-      $out[] = "   * ${action}";
-      if ($id) {
-        $out[] = "   * @param \$id string The unique identifier for this object.";
-      }
-      $out[] = "   */";
-      if ($id) {
-        $out[] = "  public function {$action}(\$id) {";
-      } else {
-        $out[] = "  public function {$action}() {";
-      }
-      $out[] = '';
-      $out[] = '}';
-      $out[] = '';
+			$out[] = '  /**';
+			$out[] = "   * ${action}";
+			if ($id) {
+				$out[] = "   * @param \$id string The unique identifier for this object.";
+			}
+			$out[] = "   */";
+			if ($id) {
+				$out[] = "  public function {$action}(\$id) {";
+			} else {
+				$out[] = "  public function {$action}() {";
+			}
+			$out[] = '';
+			$out[] = '}';
+			$out[] = '';
 
-      return implode("\n", $out);
-    }
+			return implode("\n", $out);
+		}
+
+		// @codeCoverageIgnoreStart
 
 		/**
 		 * Creates a view template file
@@ -187,14 +193,16 @@
 		private static function view($path) {
 			@touch($path);
 		}
-		
+
 		public static function update($dir) {
 			//update scripts
 			self::generate_scripts(FileUtils::join($dir));
 			//update boot.php
 			self::generate_boot(FileUtils::join($dir, '..', 'config', 'boot.php'));
 		}
-		
+
+		// @codeCoverageIgnoreEnd
+
 		/**
 		 * Create templates for mailers.
 		 * @param string $name The name of the mailer.
@@ -204,18 +212,18 @@
 			$class_name = Inflector::classify($name);
 			$out = file_get_contents(FileUtils::join(static::$template_path, 'mailer.tmpl'));
 			$template_path = FileUtils::join(static::$nimble_root, 'app', 'view', strtolower(Inflector::underscore($class_name)));
-			
+
 			$method_output = '';
 			foreach ($methods as $method) {
 				$method_output .= self::mailer_method($method);
 				self::mailer_template($template_path, $method);
 			}
-			
+
 			$out = str_replace(array('{class_name}', '{template_path}', '{methods}'), array($class_name, $template_path, $method_output), $out);
 			$path_name = FileUtils::join(static::$nimble_root, 'app', 'model', $class_name . '.php');
 			static::write_file($path_name, $out);
 		}
-		
+
 		/**
 		 * Generate the mailer method.
 		 */
@@ -229,17 +237,17 @@
 			return $out;
 		}
 
-    /**
-     * Generate a set of mailer templates, one PHP file and one text file.
-     * @access private
-     */
+		/**
+		 * Generate a set of mailer templates, one PHP file and one text file.
+		 * @access private
+		 */
 		private static function mailer_template($path, $method) {
-      FileUtils::mkdir_p($path);
+			FileUtils::mkdir_p($path);
 			foreach (array('php', 'txt') as $type) {
-  			@touch(FileUtils::join($path, strtolower($method) . '.' . $type));
+				@touch(FileUtils::join($path, strtolower($method) . '.' . $type));
 			}
 		}
-		
+
 		/**
 		 * Generate a migration.
 		 * @param string $name The name of the migration.
@@ -248,10 +256,10 @@
 		public static function generate_migration($name, $table = '') {
 			$path = FileUtils::join(static::$nimble_root, 'db', 'migrations');
 			FileUtils::mkdir_p($path);
-			
+
 			$file_name = time() . '_' . $name . '_migration.php';
 			$class_name = Inflector::classify($name . 'Migration');
-									
+
 			$out = file_get_contents(FileUtils::join(static::$template_path, 'migration.tmpl'));
 			$up = '';
 			$down = '';
@@ -259,42 +267,42 @@
 				$up .= '$t = $this->create_table("' . $table . '");';
 				$up .= "\n" . '				//enter column definitions here';
 				$up .= "\n" . '			$t->go();';
-				$down .= '	$this->drop_table("' . $table . '");';				
+				$down .= '	$this->drop_table("' . $table . '");';
 			}
 			$out = str_replace(
-			  array('{class_name}', '{up_code}', '{down_code}'), 
-			  array($class_name, $up, $down), 
-			  $out
+			array('{class_name}', '{up_code}', '{down_code}'),
+			array($class_name, $up, $down),
+			$out
 			);
-			
+
 			static::write_file(FileUtils::join($path, $file_name), $out);
 		}
-		
+
 		/**
 		 * Write a file to the filesystem.
 		 * @access private
 		 */
 		private static function write_file($path, $string) {
-		  file_put_contents($path, $string);
+			file_put_contents($path, $string);
 		}
 
-    /**
-     * Retrieve the help documentation.
-     */
+		/**
+		 * Retrieve the help documentation.
+		 */
 		public static function help() {
 			return file_get_contents(FileUtils::join(static::$template_path, 'help.tmpl'));
 		}
 	}
 
-  foreach (array(
-  	'template_path' => array(dirname(__FILE__), '..', 'templates'),
-  	'script_path' => array(dirname(__FILE__), '..', '..', 'nimble_scripts')
-  ) as $name => $path) {
-  	Generator::${$name} = FileUtils::join($path);
-  }
-  
-  if (defined('NIMBLE_ROOT')) {
-    Generator::$nimble_root = NIMBLE_ROOT; 
-  }
-  
-?>
+	foreach (array(
+		'template_path' => array(dirname(__FILE__), '..', 'templates'),
+		'script_path' => array(dirname(__FILE__), '..', '..', 'nimble_scripts')
+	) as $name => $path) {
+		Generator::${$name} = FileUtils::join($path);
+	}
+
+	if (defined('NIMBLE_ROOT')) {
+		Generator::$nimble_root = NIMBLE_ROOT;
+	}
+
+	?>
--- test/nimble_generators/GeneratorTest.php
+++ test/nimble_generators/GeneratorTest.php
@@ -11,92 +11,99 @@ class GeneratorTest extends PHPUnit_Framework_TestCase {
 		$root = new vfsStreamDirectory('root');
 		$root->addChild(new vfsStreamDirectory('template'));
 		$root->addChild(new vfsStreamDirectory('scripts'));
-		$root->addChild(new vfsStreamDirectory('target'));	
-		vfsStreamWrapper::setRoot($root);	
+		$root->addChild(new vfsStreamDirectory('target'));
+		vfsStreamWrapper::setRoot($root);
 
 		Generator::$template_path = vfsStream::url('root/template');
 		Generator::$script_path = vfsStream::url('root/scripts');
 		Generator::$nimble_root = vfsStream::url('root');
 	}
-	
+
 	function providerTestGenerateTemplate() {
 		return array(
 			array('old', false),
 			array('new', true)
-		);	
+		);
 	}
-	
+
 	/**
 	 * @covers Generator::__callStatic
 	 * @dataProvider providerTestGenerateTemplate
 	 */
 	function testGenerateTemplate($name, $is_generated) {
 		file_put_contents(vfsStream::url('root/template/new.tmpl'), "");
-		
+
 		call_user_func("Generator::generate_${name}", vfsStream::url('root/target/made-it'));
-		
+
 		$this->assertEquals($is_generated, file_exists(vfsStream::url('root/target/made-it')));
 	}
-	
+
 	/**
 	 * @covers Generator::generate_test
 	 */
 	function testGenerateTest() {
 		mkdir(vfsStream::url('root/test/unit'), 0777, true);
 		file_put_contents(vfsStream::url('root/template/unit_test.tmpl'), '{class_name}');
-		
+
 		Generator::generate_test('unit', 'Unit');
-		
+
 		$this->assertTrue(vfsStreamWrapper::getRoot()->hasChild('test/unit/UnitTest.php'));
 		$this->assertEquals("Unit", file_get_contents(vfsStream::url('test/unit/UnitTest.php')));
 	}
-	
+
 	/**
 	 * @covers Generator::generate_scripts
 	 */
 	function testGenerateScripts() {
-	  file_put_contents(vfsStream::url('root/scripts/nimblize'), 'no');	
-	  file_put_contents(vfsStream::url('root/scripts/test'), 'yes');	
-	  
+	  file_put_contents(vfsStream::url('root/scripts/nimblize'), 'no');
+	  file_put_contents(vfsStream::url('root/scripts/test'), 'yes');
+
 	  mkdir(vfsStream::url('root/test/scripts'), 0777, true);
 	  Generator::$nimble_root = vfsStream::url('root/test');
-	  
+
 	  Generator::generate_scripts(vfsStream::url('root/test/scripts'));
-	  
+
 	  $this->assertFileExists(vfsStream::url('root/test/scripts/test'));
 	  $this->assertFileNotExists(vfsStream::url('root/test/scripts/nimblize'));
 	}
-	
+
+	/**
+	 * @expectedException Exception
+	 */
+	function testGenerateScriptsNotADir() {
+	  Generator::generate_scripts(vfsStream::url('root/blah'));
+	}
+
 	/**
 	 * @covers Generator::generate_database_config
 	 */
 	function testDatabaseConfig() {
 		mkdir(vfsStream::url('root/test'), 0777, true);
 		file_put_contents(vfsStream::url('root/template/database.json'), "[env]");
-		
+
 		Generator::generate_database_config(vfsStream::url('root/test/database'), 'test');
-		
+
 		$this->assertFileExists(vfsStream::url('root/test/database'));
 		$this->assertEquals('test', file_get_contents(vfsStream::url('root/test/database')));
 	}
-	
+
 	function testGenerateModel() {
 		mkdir(vfsStream::url('root/test/unit'), 0777, true);
 		mkdir(vfsStream::url('root/app/model'), 0777, true);
 		file_put_contents(vfsStream::url('root/template/model.tmpl'), "{class_name}");
 		file_put_contents(vfsStream::url('root/template/unit_test.tmpl'), "{class_name}");
-	 	
+
 	 	Generator::generate_model("Test");
-	 	
+
 		$this->assertFileExists(vfsStream::url('root/app/model/test.php'));
 		$this->assertEquals('Test', file_get_contents(vfsStream::url('root/app/model/test.php')));
-		$this->assertFileExists(vfsStream::url('root/test/unit/TestTest.php'));	 	
+		$this->assertFileExists(vfsStream::url('root/test/unit/TestTest.php'));
 	}
 
 	function providerTestGenerateController() {
 		return array(
 			array(true), array(false)
-		);	
+		);
 	}
 
   /**
@@ -108,20 +115,20 @@ class GeneratorTest extends PHPUnit_Framework_TestCase {
     mkdir(vfsStream::url('root/app/view'), 0777, true);
     mkdir(vfsStream::url('root/test/functional'), 0777, true);
 		file_put_contents(vfsStream::url('root/template/controller.tmpl'), "{class_name}");
-    
+
     Generator::generate_controller('Test', $generate_views);
-    
+
     $this->assertFileExists(vfsStream::url('root/app/controller/test_controller.php'));
     $this->assertEquals('Test', file_get_contents(vfsStream::url('root/app/controller/test_controller.php')));
-    
-    $this->assertEquals($generate_views, file_exists(vfsStream::url('root/app/view/test'))); 
-		$this->assertFileExists(vfsStream::url('root/test/functional/TestTest.php'));	 	
+
+    $this->assertEquals($generate_views, file_exists(vfsStream::url('root/app/view/test')));
+		$this->assertFileExists(vfsStream::url('root/test/functional/TestTest.php'));
   }
 
   function providerTestViewFunction() {
     return array(
       array(false, true)
-    ); 
+    );
   }
 
   /**
@@ -132,7 +139,7 @@ class GeneratorTest extends PHPUnit_Framework_TestCase {
     $result = Generator::view_function('index', $id);
     $this->assertEquals(($id ? 1 : 0), preg_match('#\($id\)#', $result));
   }
-  
+
   /**
    * @dataProvider providerTestGenerateMailer
    * @covers Generator::generate_mailer
@@ -143,17 +150,17 @@ class GeneratorTest extends PHPUnit_Framework_TestCase {
     mkdir(vfsStream::url('root/app/model'), 0777, true);
 
     Generator::generate_mailer('Test', array('create'));
-    
+
     $this->assertFileExists(vfsStream::url('root/app/model/Test.php'));
     $this->assertEquals('Test', file_get_contents(vfsStream::url('root/app/model/Test.php')));
     $this->assertFileExists(vfsStream::url('root/app/view/test'));
   }
-  
+
   function testGenerateMigration() {
 		file_put_contents(vfsStream::url('root/template/migration.tmpl'), "{class_name}");
 
     Generator::generate_migration('Test', 'test');
-    
+
     $this->assertFileExists(vfsStream::url('root/db/migrations'));
     $dh = opendir(vfsStream::url('root/db/migrations'));
     while (($result = readdir($dh)) !== false) {
@@ -161,6 +168,13 @@ class GeneratorTest extends PHPUnit_Framework_TestCase {
       $this->assertEquals('TestMigration', file_get_contents(vfsStream::url('root/db/migrations/') . $result));
     }
     closedir($dh);
+  }
+
+  function testHelp() {
+		file_put_contents(vfsStream::url('root/template/help.tmpl'), "help");
+
+		$this->assertEquals('help', Generator::help());
+
   }
 }
 
