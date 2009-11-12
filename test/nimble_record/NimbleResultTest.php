35081ee4698327941beb5324605874641346da6b
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2009-11-11 20:29:45 -0500

    more nimble result tests

--- test/nimble_record/NimbleResultTest.php
+++ test/nimble_record/NimbleResultTest.php
@@ -9,6 +9,22 @@ require_once(dirname(__FILE__) . '/config.php');
 			$this->assertEquals($users->length(), 10);
 			$this->assertEquals($users->length, 10);
 		}
+		
+		public function testKeys() {
+			$users = User::find('all');
+			$keys = $users->keys();
+			$i = 0;
+			foreach($keys as $key) {
+				$this->assertEquals($i, $key);
+				$i++;
+			}
+		}
+		
+		public function testToString() {
+			$users = User::find('all');
+			$s = (string) $users;
+			$this->assertTrue(empty($s));
+		}
 
 	}
 ?>
